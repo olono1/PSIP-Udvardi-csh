80663dce0100e942d27016404dcc1033b12e7c94
Oliver Udvardi <oudvardi@gmail.com>
2021-03-01 19:28:41 +0100

    Starting to work o logic

--- Form1.cs
+++ Form1.cs
@@ -31,20 +31,11 @@ namespace PSIP_Udvardi_csh
         }
         private void ThreadTask()
         {
-            int stp;
-            int newval;
-            Random rnd = new Random();
-
             while (true)
             {
-                stp = this.progressBar1.Step * rnd.Next(1, 2);
-                newval = this.progressBar1.Value + stp;
-                if (newval > this.progressBar1.Maximum)
-                    newval = this.progressBar1.Maximum;
-                else if (newval < this.progressBar1.Minimum)
-                    newval = this.progressBar1.Minimum;
+
                 this.progressBar1.Invoke(new MethodInvoker(delegate { this.progressBar1.Value = newval; }));
-                Thread.Sleep(100);
+               
             }
 
         }
